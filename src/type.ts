8cc506c2538efce6564e1b74811b22a2a60ec8ce
korkt-kim <gnscott6@naver.com>
2026-01-05 21:03:55 +0900

    feat: implement GitHub PR creation logic and update hosting types

--- src/type.ts
+++ src/type.ts
@@ -1,10 +1,12 @@
-export type SetRequired<T, K extends keyof T> = Omit<T,K> & { [P in K]-?: T[P] };
+export type SetRequired<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: T[P];
+};
 export type SVGCode = {
-    name: string;
-    svg: string;
-}
+  name: string;
+  svg: string;
+};
 
 export type TSXCode = {
-    name: string;
-    tsx: string;
-}
+  name: string;
+  tsx: string;
+};
---
+++ src/utils/codeHosting/github.ts
@@ -0,0 +1,141 @@
+import { CodeHosting } from "./types";
+import { Octokit } from "@octokit/rest";
+import gh from "parse-github-url";
+import { SetRequired } from "../../type";
+import { sanitizeComponentName } from "../format";
+
+export class GitHub implements CodeHosting {
+  private octokit: Octokit | null = null;
+  constructor() {}
+
+  async createPR({
+    accessToken,
+    url,
+    destDirectory,
+    commitMessage,
+    svgs,
+  }: {
+    accessToken: string;
+    url: string;
+    destDirectory: string;
+    commitMessage: string;
+    svgs: { name: string; code: string }[];
+  }) {
+    this.octokit = new Octokit({
+      auth: accessToken,
+    });
+
+    const parsedGithubUrl = this.getParsedGithubUrl(url);
+
+    const owner = parsedGithubUrl.owner;
+    const repoName = parsedGithubUrl.name;
+    const branch = parsedGithubUrl.branch;
+
+    if (!owner || !repoName || !branch) {
+      console.error("Invalid URL. Failed to create PR");
+      return;
+    }
+
+    await this.octokit.repos.get({
+      owner,
+      repo: repoName,
+    });
+
+    let originBranchRef = null;
+
+    originBranchRef = await this.getBranchRef(owner, repoName, branch);
+
+    if (!originBranchRef) {
+      originBranchRef = await this.getBranchRef(owner, repoName, "main");
+      if (!originBranchRef) {
+        console.error("Failed to get origin branch ref main");
+        return;
+      }
+
+      await this.octokit.rest.git.createRef({
+        owner,
+        repo: repoName,
+        ref: `refs/heads/${branch}`,
+        sha: originBranchRef.data.object.sha,
+      });
+    }
+
+    const blobs = await Promise.all(
+      svgs.map(async (svg) => {
+        const blob = await this.octokit?.git.createBlob({
+          owner,
+          repo: repoName,
+          content: btoa(svg.code),
+          encoding: "base64",
+        });
+
+        return {
+          ...blob,
+          name: `${sanitizeComponentName(svg.name)}.tsx`,
+        };
+      }),
+    );
+
+    const tree = await this.octokit.git.createTree({
+      owner,
+      repo: repoName,
+      tree: (
+        blobs.filter((blob) => !!blob.data) as SetRequired<
+          (typeof blobs)[number],
+          "data"
+        >[]
+      ).map((blob) => {
+        return {
+          type: "blob",
+          sha: blob.data.sha,
+          mode: "100644",
+          path: `${destDirectory}/${blob.name}`,
+        };
+      }),
+      base_tree: originBranchRef.data.object.sha,
+    });
+
+    const commit = await this.octokit.git.createCommit({
+      owner,
+      repo: repoName,
+      message: commitMessage,
+      tree: tree.data.sha,
+      parents: [originBranchRef.data.object.sha],
+    });
+
+    await this.octokit.git.updateRef({
+      owner,
+      repo: repoName,
+      ref: `heads/${branch}`,
+      sha: commit.data.sha,
+      force: true,
+    });
+  }
+
+  private async getBranchRef(owner: string, repo: string, ref: string) {
+    if (!this.octokit) {
+      console.error("Octokit not initialized");
+      return null;
+    }
+
+    try {
+      return await this.octokit.git.getRef({
+        owner,
+        repo,
+        ref: `heads/${ref}`,
+      });
+    } catch (error) {
+      return null;
+    }
+  }
+
+  private getParsedGithubUrl(url: string) {
+    console.log("url", url);
+    const parsedUrl = gh(url);
+    console.log(parsedUrl);
+    if (!parsedUrl) {
+      throw new Error("Invalid URL");
+    }
+    return parsedUrl;
+  }
+}
