c6a3c4370cbc7c322a26d7c0c25b8ae83a50446b
korkt-kim <gnscott6@naver.com>
2026-01-05 21:06:08 +0900

    refactor: update UI entry point and migrate to new stylesheet

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,25 +1,20 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-
-const App = ()=>{
-    return <div>hel234o</div>
-}
+import * as ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "../assets/style.css";
 
 // DOM이 준비될 때까지 기다림
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initReact);
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initReact);
 } else {
-    initReact();
+  initReact();
 }
 
 function initReact() {
-    const container = document.querySelector('#react-page');
-    console.log('react-page element:', container);
-    if (container) {
-        const root = ReactDOM.createRoot(container);
-        root.render(<App />);
-    } else {
-        console.error('react-page element not found');
-    }
+  const container = document.querySelector("#react-page");
+  if (container) {
+    const root = ReactDOM.createRoot(container);
+    root.render(<App />);
+  } else {
+    console.error("react-page element not found");
+  }
 }
-
