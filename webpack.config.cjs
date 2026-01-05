d0d2f7b7411148aec5f68b1279f5ff1c20adb6e4
korkt-kim <gnscott6@naver.com>
2026-01-05 21:05:52 +0900

    chore: update tsconfig and webpack config for modern JS and node polyfills

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,6 +3,7 @@
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
@@ -19,13 +20,36 @@ module.exports = (env, argv) => ({
 
   module: {
     rules: [
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       { test: /\.css$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
       { test: /\.(png|jpg|gif|webp|svg)$/, use: [{ loader: 'url-loader' }] },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
   },
 
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    fallback: {
+      process: require.resolve('process/browser'),
+      assert: require.resolve('assert/'),
+      stream: require.resolve('stream-browserify'),
+      string_decoder: require.resolve('string_decoder/'),
+      os: require.resolve('os-browserify/browser'),
+      url: require.resolve('url/'),
+      buffer: require.resolve('buffer/'),
+      path: require.resolve('path-browserify'),
+      fs: false,
+    },
+  },
 
   output: {
     filename: '[name].js',
@@ -33,6 +57,10 @@ module.exports = (env, argv) => ({
   },
 
   plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
     new HtmlWebpackPlugin({
       template: './src/ui.html',
       filename: 'ui.html',
