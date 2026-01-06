dbefb987a6ac26b321f6e82eaee42c9af463948a
korkt-kim <gnscott6@naver.com>
2026-01-06 22:57:28 +0900

    feat: add DoneStep to indicate successful export

---
+++ src/components/DoneStep.tsx
@@ -0,0 +1,11 @@
+import { CommonLayout } from "../layouts/CommonLayout";
+
+export const DoneStep = () => {
+  return (
+    <CommonLayout>
+      <CommonLayout.Content>
+        <p>Icons successfully added to new branch</p>
+      </CommonLayout.Content>
+    </CommonLayout>
+  );
+};
