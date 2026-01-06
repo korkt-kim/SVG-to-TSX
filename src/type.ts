b582b406c26df35382cb298e346349e72ff52c1d
korkt-kim <gnscott6@naver.com>
2026-01-06 21:10:28 +0900

    refactor: improve post-message communication system with unified event types

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,5 @@
+import { POST_MESSAGE_TYPE } from "./consts";
+
 export type SetRequired<T, K extends keyof T> = Omit<T, K> & {
   [P in K]-?: T[P];
 };
@@ -10,3 +12,21 @@ export type TSXCode = {
   name: string;
   tsx: string;
 };
+
+export type PluginMessage =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    | { type: 'loadAll'; data: Record<string, any> }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    | { type: 'save'; data: Record<string, any> }
+    | { type: 'close'; };
+
+export interface PostMessage {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  loadAll: { type: typeof POST_MESSAGE_TYPE.LOAD_ALL; data: Record<string, any> }
+  loadSvg: { type: typeof POST_MESSAGE_TYPE.LOAD_SVG; data: SVGCode[] }
+}
+    
+
+export type FigmaMessageEvent = MessageEvent<{
+  pluginMessage: PluginMessage;
+}>;
