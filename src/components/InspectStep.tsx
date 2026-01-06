aec0912d37176dd2af6108c4fe1bf561781fec5d
korkt-kim <gnscott6@naver.com>
2026-01-06 21:10:27 +0900

    feat: introduce CommonLayout and refactor UI structure

--- src/components/InspectStep.tsx
+++ src/components/InspectStep.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { useGlobalContext } from "./GlobalProvider";
 import { SVGR } from "../utils/svgr";
 import { TSXCode } from "../type";
+import { CommonLayout } from "../layouts/CommonLayout";
 
 export const InspectStep = () => {
   const globalContext = useGlobalContext();
@@ -11,12 +12,17 @@ export const InspectStep = () => {
     svgrRef.current = new SVGR();
   }
 
-  return (
-    <div>
-      {globalContext?.svgCodes.map((svg, index) => (
-        <SVGItem key={index} name={svg.name} />
-      ))}
+  const isSvgComponentSelected = globalContext.svgCodes.length > 0
 
+  return (
+    <CommonLayout>
+      <CommonLayout.Content>
+      {isSvgComponentSelected ? globalContext?.svgCodes.map((svg, index) => (
+        <SVGItem key={index} name={svg.name} />
+      )): <><p>No Component(or Instance) icons are selected.</p>
+      <a target='_blank' href="https://help.figma.com/hc/en-us/articles/360038662654-Guide-to-components-in-Figma">What are Components and Instances?</a></>}
+</CommonLayout.Content>
+      <CommonLayout.Bottom>
       <button
         onClick={async () => {
           const tsxCodes = await Promise.all(
@@ -39,7 +45,8 @@ export const InspectStep = () => {
       >
         Convert and Export
       </button>
-    </div>
+      </CommonLayout.Bottom>
+    </CommonLayout>
   );
 };
 
